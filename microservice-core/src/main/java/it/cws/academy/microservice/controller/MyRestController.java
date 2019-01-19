d298fb719a0d1fa7a86de0d813008f5fb6958fa4
Dmitrij Colautti <dmitrij.colautti@cws.it>
2019-01-19 11:52:44 +0100

    fix

--- microservice-core/src/main/java/it/cws/academy/microservice/controller/MyRestController.java
+++ microservice-core/src/main/java/it/cws/academy/microservice/controller/MyRestController.java
@@ -3,9 +3,13 @@
  */
 package it.cws.academy.microservice.controller;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +26,18 @@ import it.cws.academy.microservice.service.PersonRepository;
  *
  */
 @RestController
-public class PersonController {
-	private static final Logger log = LoggerFactory.getLogger(PersonController.class);
+public class MyRestController {
+	private static final Logger log = LoggerFactory.getLogger(MyRestController.class);
+	
+	@Value("${config.hello}")
+	String name;
+	
+	@GetMapping(path="/rest")
+	public String rest(@RequestParam(value="name", required=false, defaultValue="world") String name) {
+		log.info("called rest:"+name);
+		return "Hello " +name + "! "+LocalDateTime.now().toString();
+		
+	}
 	
 	@Autowired
 	private PersonRepository repo;
